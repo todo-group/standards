7e59aa4089c1e2d1ea477abe6b3cd36085223fa9
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2016-11-10 09:48:37 +0900

    introduced core.cmake

---
+++ doc/preamble.cpp
@@ -0,0 +1,4 @@
+// Copyright (C) YYYY-YYYY by Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
