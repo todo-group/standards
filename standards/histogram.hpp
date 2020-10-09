8d0949da7c2dc4cdefa96edd4f47b88bcc7c65c8
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2020-10-09 19:09:35 +0900

    Change license to Apache 2.0

--- standards/histogram.hpp
+++ standards/histogram.hpp
@@ -1,10 +1,20 @@
-// Copyright (C) 2018 by Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+/*
+   Copyright (C) 2015-2020 by Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
 
-#ifndef STANDARDS_HISTOGRAM_HPP
-#define STANDARDS_HISTOGRAM_HPP
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#pragma once
 
 #include <cstdint>
 #include <stdexcept>
@@ -55,5 +65,3 @@ inline std::ostream& operator<<(std::ostream& os, histogram const& hist) {
 }
 
 } // end namespace standards
-
-#endif // STANDARDS_HISTOGRAM_HPP
