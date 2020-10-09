e99103f3dd900205d9faecaae68724bd8def9e54
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2020-10-09 17:33:51 +0900

    Fix

--- config/setup.sh
+++ config/setup.sh
@@ -7,6 +7,6 @@ BASE_DIR=$(cd "$(dirname $0)"; cd ..; pwd)
 # googletest
 
 rm -rf $BASE_DIR/googletest*
-curl -L https://github.com/google/googletest/archive/release-${GOOGLETEST_VERSION}.tar.gz | tar xf - -C ${BASE_DIR}
+curl -L https://github.com/google/googletest/archive/release-${GOOGLETEST_VERSION}.tar.gz | tar zxf - -C ${BASE_DIR}
 mv -f ${BASE_DIR}/googletest-release-${GOOGLETEST_VERSION}/googletest ${BASE_DIR}
 rm -rf ${BASE_DIR}/googletest-release-${GOOGLETEST_VERSION}
