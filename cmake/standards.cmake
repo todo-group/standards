b2bb81287c8db6649d6bfce52b3ea933394d7450
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2021-12-08 09:34:19 +0900

    update

--- cmake/standards.cmake
+++
@@ -1,9 +0,0 @@
-option(STANDARDS_INSTALL OFF)
-option(STANDARDS_BUILD_TESTS OFF)
-option(STANDARDS_BUILD_EXAMPLES OFF)
-
-include(FetchContent)
-FetchContent_Declare(
-  standards
-  GIT_REPOSITORY https://github.com/todo-group/standards.git
-)
